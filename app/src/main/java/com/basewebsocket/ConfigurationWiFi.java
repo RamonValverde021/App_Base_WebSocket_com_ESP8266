a4d4f0e2bb3b93214081e9fce08d88be2a1d97be
Ramon_Valverde <ramonaguiar1998@gmail.com>
2025-07-19 18:12:52 -0300

    upload

--- app/src/main/java/com/basewebsocket/ConfigurationWiFi.java
+++ app/src/main/java/com/basewebsocket/ConfigurationWiFi.java
@@ -1,8 +1,12 @@
 package com.basewebsocket;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class ConfigurationWiFi extends AppCompatActivity {
 
     Intent tela;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,7 @@ public class ConfigurationWiFi extends AppCompatActivity {
         });
 
         tela = new Intent(getApplicationContext(), MainActivity.class);
+        btnEnviar = findViewById(R.id.btnSaveConfig);
     }
 
     public void returnMain(View view) {
@@ -33,8 +41,45 @@ public class ConfigurationWiFi extends AppCompatActivity {
     }
 
     public void saveConfig(View view) {
+        String ssid = ((EditText) findViewById(R.id.txtSSID)).getText().toString();
+        String password = ((EditText) findViewById(R.id.txtPassword)).getText().toString();
+        String ipAddress = ((EditText) findViewById(R.id.txtIPAddress)).getText().toString();
+        String subnetMask = ((EditText) findViewById(R.id.txtSubnetMask)).getText().toString();
+        String defaultGateway = ((EditText) findViewById(R.id.txtDefaultGateway)).getText().toString();
+        String dns = ((EditText) findViewById(R.id.txtDNS)).getText().toString();
+
+        String mensagem = "Confirmação:\n\n"
+                + "SSID: " + ssid + "\n"
+                + "Senha: " + password + "\n"
+                + "IP: " + ipAddress + "\n"
+                + "Sub-rede: " + subnetMask + "\n"
+                + "Gateway: " + defaultGateway + "\n"
+                + "DNS: " + dns;
+/*
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Confirmação")
+                .setMessage(mensagem)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    Toast.makeText(this, "Configurações salvas!", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
+                */
+        new AlertDialog.Builder(this)
+                .setTitle("Confirmação")
+                .setMessage(mensagem)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    // ação ao confirmar
+                    Toast.makeText(this, "Configurações salvas!", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton("Cancelar", null)
+                .show();
+
     }
 
     public void lookPassword(View view) {
+    }
+
+    public void viewTutorial(View view) {
     }
 }
